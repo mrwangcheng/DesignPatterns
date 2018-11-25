b42655f49a3d2b722ddc3102579566ec3dabce79
My Name <my-name@chromium.org>
2018-11-25 15:39:44 +0800

    add brigde desing

---
+++ Bridge_Design/Bridge_Design.cpp
@@ -0,0 +1,16 @@
+// Bridge_Design.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Abstraction.h"
+#include "AbstractionImp.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	AbstractionImp* imp = new ConcreteAbstractionImpA();
+	Abstraction* abs = new RefinedAbstraction(imp);
+	abs->Operation();
+	return 0;
+}
+
