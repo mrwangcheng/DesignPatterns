935f62ad6e0f7002a8e3d8cafd2922a9eac399ee
My Name <my-name@chromium.org>
2018-12-09 15:29:22 +0800

    mogify

--- Compsonment_Design/Compsonment_Design.cpp
+++ Compsonment_Design/Compsonment_Design.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	unique_ptr<Leaf> l(new Leaf());
 	l->Operation();
 	unique_ptr<Composite> com(new Composite());
-	com->Add(l.get());
+	com->Add(l.release());
 	com->Operation();
 	unique_ptr<CompositeComponent> ll(com->GetChild(0));
 	ll->Operation();
